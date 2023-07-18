aae153412551e6d48efef30ce19c975edfcb57d1
joaquingonjua <130697832+joaquingonjua@users.noreply.github.com>
2023-07-18 16:01:21 -0300

    Agrega archivos del frontend y backend

---
+++ back/webapi/Migrations/20230713234344_init.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Partidas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    P1Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    P2Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Partidas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Partidas");
+        }
+    }
+}
---
+++ back/webapi/Models/AplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿//Esta clase nos permite crear la BBDD y poder trabajar con ella
+
+using Microsoft.EntityFrameworkCore;
+
+namespace webapi.Models
+{
+    public class AplicationDbContext: DbContext //Decimos que la clase AplicationDbContext va a heredar la clas DbContext de EF Core
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options) 
+        {
+                
+        }
+
+        public DbSet<Partida> Partidas { get; set; }
+
+        public DbSet<Ronda> Rondas { get; set; }
+
+}
+}
